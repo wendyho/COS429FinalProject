252692d723155fe8ac325e07d305e0fa18829013
Koert Chen <koert0chen@gmail.com>
2020-12-07 19:34:48 -0500

    Reorganization update.

---
+++ code/object_detection_model.py
@@ -0,0 +1,123 @@
+import io
+import os
+import scipy.misc
+import numpy as np
+import six
+import time
+
+from six import BytesIO
+
+import matplotlib
+import matplotlib.pyplot as plt
+from PIL import Image, ImageDraw, ImageFont
+
+import tensorflow as tf
+import tensorflow_hub as hub
+from object_detection.utils import visualization_utils as viz_utils
+from object_detection.utils import label_map_util
+
+class ObjectDetectionModel:
+    def __init__(self):
+        PATH_TO_LABELS = 'models/research/object_detection/data/mscoco_label_map.pbtxt'
+        self.category_index = label_map_util.create_category_index_from_labelmap(PATH_TO_LABELS, use_display_name=True)
+        
+        tf.keras.backend.clear_session()
+#         self.detect_fn = tf.saved_model.load('models/research/object_detection/test_data/efficientdet_d5_coco17_tpu-32/saved_model')
+        
+        ALL_MODELS = {
+        'CenterNet HourGlass104 512x512' : 'https://tfhub.dev/tensorflow/centernet/hourglass_512x512/1',
+        'CenterNet HourGlass104 Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/hourglass_512x512_kpts/1',
+        'CenterNet HourGlass104 1024x1024' : 'https://tfhub.dev/tensorflow/centernet/hourglass_1024x1024/1',
+        'CenterNet HourGlass104 Keypoints 1024x1024' : 'https://tfhub.dev/tensorflow/centernet/hourglass_1024x1024_kpts/1',
+        'CenterNet Resnet50 V1 FPN 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512/1',
+        'CenterNet Resnet50 V1 FPN Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v1_fpn_512x512_kpts/1',
+        'CenterNet Resnet101 V1 FPN 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet101v1_fpn_512x512/1',
+        'CenterNet Resnet50 V2 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512/1',
+        'CenterNet Resnet50 V2 Keypoints 512x512' : 'https://tfhub.dev/tensorflow/centernet/resnet50v2_512x512_kpts/1',
+        'EfficientDet D0 512x512' : 'https://tfhub.dev/tensorflow/efficientdet/d0/1',
+        'EfficientDet D1 640x640' : 'https://tfhub.dev/tensorflow/efficientdet/d1/1',
+        'EfficientDet D2 768x768' : 'https://tfhub.dev/tensorflow/efficientdet/d2/1',
+        'EfficientDet D3 896x896' : 'https://tfhub.dev/tensorflow/efficientdet/d3/1',
+        'EfficientDet D4 1024x1024' : 'https://tfhub.dev/tensorflow/efficientdet/d4/1',
+        'EfficientDet D5 1280x1280' : 'https://tfhub.dev/tensorflow/efficientdet/d5/1',
+        'EfficientDet D6 1280x1280' : 'https://tfhub.dev/tensorflow/efficientdet/d6/1',
+        'EfficientDet D7 1536x1536' : 'https://tfhub.dev/tensorflow/efficientdet/d7/1',
+        'SSD MobileNet v2 320x320' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2',
+        'SSD MobileNet V1 FPN 640x640' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v1/fpn_640x640/1',
+        'SSD MobileNet V2 FPNLite 320x320' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/fpnlite_320x320/1',
+        'SSD MobileNet V2 FPNLite 640x640' : 'https://tfhub.dev/tensorflow/ssd_mobilenet_v2/fpnlite_640x640/1',
+        'SSD ResNet50 V1 FPN 640x640 (RetinaNet50)' : 'https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_640x640/1',
+        'SSD ResNet50 V1 FPN 1024x1024 (RetinaNet50)' : 'https://tfhub.dev/tensorflow/retinanet/resnet50_v1_fpn_1024x1024/1',
+        'SSD ResNet101 V1 FPN 640x640 (RetinaNet101)' : 'https://tfhub.dev/tensorflow/retinanet/resnet101_v1_fpn_640x640/1',
+        'SSD ResNet101 V1 FPN 1024x1024 (RetinaNet101)' : 'https://tfhub.dev/tensorflow/retinanet/resnet101_v1_fpn_1024x1024/1',
+        'SSD ResNet152 V1 FPN 640x640 (RetinaNet152)' : 'https://tfhub.dev/tensorflow/retinanet/resnet152_v1_fpn_640x640/1',
+        'SSD ResNet152 V1 FPN 1024x1024 (RetinaNet152)' : 'https://tfhub.dev/tensorflow/retinanet/resnet152_v1_fpn_1024x1024/1',
+        'Faster R-CNN ResNet50 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_640x640/1',
+        'Faster R-CNN ResNet50 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_1024x1024/1',
+        'Faster R-CNN ResNet50 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet50_v1_800x1333/1',
+        'Faster R-CNN ResNet101 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_640x640/1',
+        'Faster R-CNN ResNet101 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_1024x1024/1',
+        'Faster R-CNN ResNet101 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet101_v1_800x1333/1',
+        'Faster R-CNN ResNet152 V1 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_640x640/1',
+        'Faster R-CNN ResNet152 V1 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_1024x1024/1',
+        'Faster R-CNN ResNet152 V1 800x1333' : 'https://tfhub.dev/tensorflow/faster_rcnn/resnet152_v1_800x1333/1',
+        'Faster R-CNN Inception ResNet V2 640x640' : 'https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_640x640/1',
+        'Faster R-CNN Inception ResNet V2 1024x1024' : 'https://tfhub.dev/tensorflow/faster_rcnn/inception_resnet_v2_1024x1024/1',
+        'Mask R-CNN Inception ResNet V2 1024x1024' : 'https://tfhub.dev/tensorflow/mask_rcnn/inception_resnet_v2_1024x1024/1'
+        }
+        model_display_name = 'CenterNet HourGlass104 Keypoints 512x512' 
+        model_handle = ALL_MODELS[model_display_name]
+        
+        self.COCO17_HUMAN_POSE_KEYPOINTS = [(0, 1),
+            (0, 2),
+            (1, 3),
+            (2, 4),
+            (0, 5),
+            (0, 6),
+            (5, 7),
+            (7, 9),
+            (6, 8),
+            (8, 10),
+            (5, 6),
+            (5, 11),
+            (6, 12),
+            (11, 12),
+            (11, 13),
+            (13, 15),
+            (12, 14),
+            (14, 16)]
+    
+        self.detect_fn = hub_model = hub.load(model_handle)
+
+    def inference(self, image_np, logging=False):
+        image_np = np.expand_dims(image_np, axis=0)
+        detections = self.detect_fn(image_np)
+        detections = {key:value.numpy() for key,value in detections.items()}
+        
+        if 'detection_keypoints' in detections:
+            keypoints = detections['detection_keypoints'][0]
+            keypoint_scores = detections['detection_keypoint_scores'][0]
+
+        if logging:
+            label_id_offset = 0
+            image_np_with_detections = image_np.copy()
+            viz_utils.visualize_boxes_and_labels_on_image_array(
+                image_np_with_detections[0],
+                detections['detection_boxes'][0],
+                (detections['detection_classes'][0] + label_id_offset).astype(int),
+                detections['detection_scores'][0],
+                self.category_index,
+                use_normalized_coordinates=True,
+                max_boxes_to_draw=200,
+                min_score_thresh=.30,
+                agnostic_mode=False,
+                keypoints=keypoints,
+                keypoint_scores=keypoint_scores,
+                keypoint_edges=self.COCO17_HUMAN_POSE_KEYPOINTS)
+
+            plt.figure(figsize=(24,32))
+            plt.imshow(image_np_with_detections[0])
+            plt.show()
+
+        return detections
+        
---
+++ code/quick_draw_dataset.py
@@ -0,0 +1,329 @@
+from quickdraw import QuickDrawData
+from quickdraw import QuickDrawDataGroup
+import IPython
+import os
+import json
+import random
+import json
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import requests
+from io import BytesIO
+import base64
+from math import trunc
+from PIL import Image as PILImage
+from PIL import ImageDraw as PILImageDraw
+from pycocotools.coco import COCO
+import numpy as np
+import skimage.io as io
+import matplotlib.pyplot as plt
+import pylab
+from object_detection.utils import label_map_util
+
+class QuickDrawDataset:
+    def __init__(self):
+        self.qd = QuickDrawData()
+        self.coco_to_quickdraw = {
+            "pizza": "pizza",
+            "bus": "bus",
+            "couch": "couch",
+            "remote": "key",
+            "snowboard": "skateboard",
+            "fork": "fork",
+            "suitcase": "suitcase",
+            "scissors": "scissors",
+            "sheep": "sheep",
+#             "person": "scorpion",
+            "person": "teddy-bear",
+            "dog": "dog", 
+            "cake": "cake",
+            "tv": "television",
+            "frisbee": "baseball",
+            "carrot": "carrot",
+            "bird": "bird",
+            "bicycle": "bicycle",
+            "cow": "cow",
+            "cat": "cat",
+            "bowl": "bathtub",
+            "book": "book",
+            "tie": "bowtie",
+            "sink": "sink",
+            "toothbrush": "toothbrush",
+            "bed": "bed",
+            "bear": "bear",
+            "skateboard": "skateboard",
+            "train": "train",
+            "backpack": "backpack",
+            "cup": "cup",
+            "bottle": "wine bottle",
+            "car": "car",
+            "laptop": "laptop",
+            "elephant": "elephant",
+            "bench": "bench",
+            "knife": "knife",
+            "zebra": "zebra",
+            "orange": "apple",
+            "donut": "donut",
+            "apple": "apple",
+            "chair": "chair",
+            "toilet": "toilet",
+            "banana": "banana",
+            "airplane": "airplane",
+            "giraffe": "giraffe",
+            "refrigerator": "cooler",
+            "broccoli": "broccoli", 
+            "clock": "clock", 
+            "spoon": "fork", 
+            "handbag": "purse", 
+            "horse": "horse", 
+            "toaster": "toaster", 
+            "microwave": "microwave", 
+            "motorcycle": "motorbike", 
+            "vase": "vase", 
+            "keyboard": "keyboard", 
+            "skis": "lollipop", 
+            "oven": "oven", 
+            "kite": "postcard", 
+            "surfboard": "skateboard", 
+            "sandwich": "sandwich", 
+            "truck": "truck", 
+            "boat": "speedboat", 
+            "umbrella": "umbrella", 
+            "mouse": "mouse", 
+            "traffic light": "traffic light", 
+            "fire hydrant": "fire hydrant", 
+            "stop sign": "stop sign", 
+            "parking meter": "sword", 
+            "sports ball": "soccer ball", 
+            "baseball bat": "baseball bat", 
+            "baseball glove": "hand", 
+            "tennis racket": "tennis racquet", 
+            "wine glass": "wine glass", 
+            "hot dog": "hot dog", 
+            "potted plant": "house plant", 
+            "dining table": "table", 
+            "cell phone": "cell phone", 
+            "teddy bear": "teddy-bear", 
+            "hair dryer": "drill" }
+      
+        PATH_TO_LABELS = 'models/research/object_detection/data/mscoco_label_map.pbtxt'
+        self.category_index = label_map_util.create_category_index_from_labelmap(
+            PATH_TO_LABELS, use_display_name=True)
+        self.norms = []
+        
+    # Returns smallest axis-aligned rectangle containing points. Input points are in fractional (Object Detection API) format.
+    # Output box is in pixel (COCO dataset annotation) format.
+    def surrounding_box(self, points, image_shape):
+        min_x = min([point[0] for point in points])
+        max_x = max([point[0] for point in points])
+        min_y = min([point[1] for point in points])
+        max_y = max([point[1] for point in points])
+        
+        surrounding_bbox = [min_y * image_shape[1],
+                    min_x * image_shape[0],
+                    (max_y - min_y) * image_shape[1],
+                    (max_x - min_x) * image_shape[0]]
+        
+        return surrounding_bbox
+        
+        
+
+    # Removes detections that either do not have a quickdraw equivalent annotations or have a score
+    # below the threshold. Returns pruned detections, used for drawing, and mask detections, used
+    # for masking.
+    def prune_detections(self, image_shape, detections, threshold):
+        pruned_detections = []
+        mask_detections = []
+        body_detections = []
+        for i in range(int(detections['num_detections'][0])):
+            if ((self.category_index[detections['detection_classes'][0][i]]['name'] in self.coco_to_quickdraw) and
+                (detections['detection_scores'][0][i] > threshold)):
+                old_bbox = detections['detection_boxes'][0][i]
+                quickdraw_class = self.coco_to_quickdraw[self.category_index[detections['detection_classes'][0][i]]['name']]
+                new_bbox = [old_bbox[1] * image_shape[1],
+                            old_bbox[0] * image_shape[0],
+                            (old_bbox[3] - old_bbox[1]) * image_shape[1],
+                            (old_bbox[2] - old_bbox[0]) * image_shape[0]]
+                mask_detections.append({'class': quickdraw_class, 'bbox': new_bbox})
+                    
+                if self.category_index[detections['detection_classes'][0][i]]['name'] == 'person':
+                    # Head
+                    head_keypoints = [0, 1, 2, 3, 4, 5, 6] # nose, left/right eyes, left/right ears, left/right shoulder
+                    head_points = [detections['detection_keypoints'][0][i][j] for j in head_keypoints]
+#                     head_top = old_bbox[1]
+#                     head_bottom = max(detections['detection_keypoints'][0][i][5][1],
+#                                    detections['detection_keypoints'][0][i][6][1])
+#                     print('head top+bot: ', head_top, head_bottom)
+#                     head_bbox = self.surrounding_box(head_points, image_shape, head_top, head_bottom)
+                    head_bbox = self.surrounding_box(head_points, image_shape)
+                    pruned_detections.append({'class': 'face', 'bbox': head_bbox})
+                    body_detections.append({'class': 'face', 'bbox': head_bbox})
+                    
+                    # Torso
+                    torso_keypoints = [5, 6, 7, 8, 11, 12] # left/right shoulder, left/right elbow, left/right hip
+                    torso_points = [detections['detection_keypoints'][0][i][j] for j in torso_keypoints]
+                    torso_bbox = self.surrounding_box(torso_points, image_shape)
+                    pruned_detections.append({'class': 't-shirt', 'bbox': torso_bbox})
+                    body_detections.append({'class': 't-shirt', 'bbox': torso_bbox})
+                    
+                    # Legs
+                    legs_keypoints = [11, 12, 13, 14, 15, 16] # left/right hip, left/right knee, left/right ankle
+                    legs_points = [detections['detection_keypoints'][0][i][j] for j in legs_keypoints]
+                    legs_bbox = self.surrounding_box(legs_points, image_shape)
+                    pruned_detections.append({'class': 'pants', 'bbox': legs_bbox})
+                    body_detections.append({'class': 'pants', 'bbox': legs_bbox})
+                else: 
+                    pruned_detections.append({'class': quickdraw_class,'bbox': new_bbox})
+    
+        return pruned_detections, mask_detections, body_detections
+    
+    # get dimensions of drawing
+    def get_quickdraw_dims(self, drawing):
+        smallest_x = float('inf')
+        smallest_y = float('inf')
+        largest_x = 0
+        largest_y = 0
+
+        for stroke in drawing.strokes:
+            xarr = []
+            yarr = []
+
+            for x, y in stroke:
+                xarr.append(x)
+                yarr.append(y)
+ 
+                if(x <= smallest_x):
+                    smallest_x = x
+                if(y <= smallest_y):
+                    smallest_y = y
+                if(x >= largest_x):
+                    largest_x = x
+                if(y >= largest_y):
+                    largest_y = y
+
+        width = largest_x-smallest_x
+        height = largest_y - smallest_y
+
+        return width, height
+
+    
+    def draw_blank(self, drawing, image, bbox_x, bbox_y, bbox_w, bbox_h):
+        I = np.zeros(image.shape)
+        width, height = self.get_quickdraw_dims(drawing)
+
+        for stroke in drawing.strokes:
+            xarr = []
+            yarr = []
+            for x, y in stroke:
+                xarr.append(x)
+                yarr.append(y)
+
+            newx = [x * (bbox_w/width) + bbox_x for x in xarr]
+            newy = [y * (bbox_h/height) + bbox_y for y in yarr]
+            newx = np.rint(newx).astype(int)
+            newy = np.rint(newy).astype(int)
+            for i in range(0, len(newx) - 1):
+                I = cv2.line(I, (newx[i],newy[i]), (newx[i+1],newy[i+1]), (255, 255, 255) , 1)
+        return I
+    
+    # Returns nearest neighbor to image (inv) from quick draw data group (qdg) of the same class.
+    # Adds augmented (rotated and flipped images)
+    def nearest_neighbor(self, qdg, inv, bbox_x, bbox_y, w, h, logging=False):
+        smallest_norm = float('inf')
+        closest_drawing = qdg.get_drawing()
+
+        for drawing in qdg.drawings:
+            drawn_image = self.draw_blank(drawing, inv, bbox_x, bbox_y, w, h)
+            drawn_image = cv2.GaussianBlur(drawn_image,(5,5),cv2.BORDER_DEFAULT)
+#             drawn_image = cv2.bitwise_not(drawn_image)
+            fnorm = np.linalg.norm(drawn_image - inv, 'fro')
+            self.norms.append(fnorm)
+
+            if(fnorm < smallest_norm):
+                if logging:
+                    plt.imshow(drawn_image, cmap='Greys')
+                    plt.axis('off')
+                    plt.show() 
+                smallest_norm = fnorm
+#                 closest_drawing = drawing.key_id
+                closest_drawing = drawing
+
+#         final = qdg.search_drawings(key_id=closest_drawing)[0]
+        return closest_drawing
+#         return final
+    
+    # Draws pruned detections on image.
+    def draw(self, image, orig_image, detections, save_filename, logging=False):
+        I = image.copy()
+        Im = orig_image.copy()
+
+        for detection in detections:
+
+            plt.axis('off')
+
+            [bbox_x, bbox_y, bbox_w, bbox_h] = detection['bbox']
+
+            bbox_x = int(bbox_x)
+            bbox_y = int(bbox_y)
+            bbox_w = int(bbox_w)
+            bbox_h = int(bbox_h)
+            
+            if bbox_w == 0 or bbox_h == 0:
+                continue
+                
+#             print('bbox: ', bbox_x, bbox_y, bbox_w, bbox_h)
+            bbox_image = Im[bbox_y:bbox_y+bbox_h, bbox_x:bbox_x+bbox_w]
+            edges = cv2.Canny(bbox_image, 50, 400, apertureSize = 3, L2gradient = True)
+            inv = edges
+#             inv = cv2.bitwise_not(edges)
+#             print('inv shape', inv.shape)
+            h, w = np.shape(inv)
+#             h, w = inv.shape
+            masked_bbox_image = np.full((I.shape[0], I.shape[1]), 0, dtype=float)
+#             print('exp shape', masked_bbox_image[bbox_y:bbox_y+bbox_h, bbox_x:bbox_x+bbox_w].shape)
+            masked_bbox_image[bbox_y:bbox_y+bbox_h, bbox_x:bbox_x+bbox_w] = inv
+#             print('masked_bbox_image')
+            masked_bbox_image = cv2.GaussianBlur(masked_bbox_image,(5,5),cv2.BORDER_DEFAULT)
+    
+#             if logging:
+#                 plt.imshow(masked_bbox_image, cmap='Greys')
+#                 plt.axis('off')
+#                 plt.show()   
+
+            qdg = QuickDrawDataGroup(detection['class'], recognized=True)
+
+            final = qdg.get_drawing()
+#             final = self.nearest_neighbor(qdg, masked_bbox_image, bbox_x, bbox_y, w, h, logging=False)
+#             final = cv2.bitwise_not(final)
+            
+            width, height = self.get_quickdraw_dims(final)
+
+            for stroke in final.strokes:
+                xarr = []
+                yarr = []
+                for x, y in stroke:
+                    xarr.append(x)
+                    yarr.append(y)
+
+                newx = [x * (bbox_w/width) + bbox_x for x in xarr]
+                newy = [y * (bbox_h/height) + bbox_y for y in yarr]
+                newx = np.rint(newx).astype(int)
+                newy = np.rint(newy).astype(int)
+                for i in range(0, len(newx) - 1):
+                    I = cv2.line(I, (newx[i],newy[i]), (newx[i+1],newy[i+1]), (255, 255, 255) , 2)
+
+        if logging:
+            plt.imshow(I)
+            plt.axis('off')
+            plt.savefig(save_filename, bbox_inches='tight', pad_inches=0)
+            plt.show()                                                
+            
+        
+        return I
+    
+    def plot_histogram(self):
+        plt.hist(self.norms, density=False, bins=10)  # `density=False` would make counts
+        plt.ylabel('Count')
+        plt.xlabel('Frobenius Norm Distance')
+        plt.show()   
